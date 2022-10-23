ba3ba3f703c0ce08a205a1df66a2e8e28c95fe97
Vladimir Ivanov <ivanov.vladimir102002@gmail.com>
2022-10-23 22:10:28 +0300

    delete build

--- server/build/game/cell-coordinate.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class CellCoordinate {
-    constructor(row = 0, column = 0) {
-        this.row = row;
-        this.column = column;
-    }
-}
-exports.default = CellCoordinate;
